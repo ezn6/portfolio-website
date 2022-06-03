2353803b380f4397796c47eeaf89665cae49bad5
ezn6 <kcourtney@naver.com>
2022-06-03 14:50:46 +0900

    JS : Scroll to section

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-
 document.addEventListener('scroll', () => {
   // console.log(window.scrollY);
   if (window.scrollY > navbarHeight) {
@@ -11,4 +10,15 @@ document.addEventListener('scroll', () => {
   } else {
     navbar.classList.remove('navbar--dark');
   }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const id = event.target.dataset.link;
+  const section = document.querySelector(id);
+  if (section == null) {
+    return;
+  }
+  section.scrollIntoView({ behavior: 'smooth' });
 });
